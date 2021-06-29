e8e9024b298050106d65cd3036c52a136c63dd48
priyaaditya10 <priyaaditya2207@gmail.com>
2021-06-29 22:49:42 +0530

    updated all the components

--- todoapp/src/components/Todo.js
+++ todoapp/src/components/Todo.js
@@ -1,1 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoForm from './TodoForm';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
+import { FaEdit} from 'react-icons/fa';
+
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
+    const [edit, setEdit] = useState({
+        id: null,
+        value: ''
+    });
+
+    const submitUpdate = value => {
+        updateTodo(edit.id, value);
+        setEdit({
+            id: null,
+            value: ''
+        });
+    };
+
+    if (edit.id) {
+        return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+    }
+
+    return todos.map((todo, index) => (
+        <div
+            className={todo.isComplete ? 'todo-row complete' : 'todo-row'} // checking if a todo is complete or not
+            key={index}>
+            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+                {todo.text}
+            </div>
+            <div className='icons'>
+                <RiDeleteBin5Fill onClick ={()=> removeTodo(todo.id)} className="remove-icon" />
+                <FaEdit onClick={()=> setEdit({id: todo.id, value: todo.text})} className="edit-icon" />
+            </div>
+        </div>
+    ));
+};
+
+export default Todo;
--- todoapp/src/components/TodoList.js
+++ todoapp/src/components/TodoList.js
@@ -1,29 +1,63 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import TodoForm from './TodoForm';
-
+import Todo from './Todo';
 
 function TodoList() {
-    const [todos,setTodos] =useState([]); // useState hook
-    
-    const addTodo = todo =>{
-     if(!todo.text || /^\s*$/.test(todo.text))
-     {
-       return;
-     }
+    const [todos, setTodos] = useState([]); // using react useState hooks
 
-     const newTodos = [todo, ...todos];// using spread operator to add items of todos array in newTodos
-     setTodos(newTodos);
-     console.log(...todos);
-     
+    // function for adding new todo item
+    const addTodo = todo => {
+        // check if user entered spaces for todo or left it blank
+        if (!todo.text || /^\s*$/.test(todo.text)) {
+            return;
+        }
+
+        const newTodos = [todo, ...todos]; // using spread operator to append todos array items with todo
+
+        setTodos(newTodos);   // updating new todos to todos array
+        console.log(...todos);
+    };
+
+    // function to remove items from todo list
+    const removeTodo = id => {
+        // checking and passing all todoitems to removedItem whose id do not match with element which is removed id
+        const removedItem = [...todos].filter(todo => todo.id !== id);
+
+        setTodos(removedItem);
+    };
+
+    // function to edit value of and todoitem
+    const updateTodo =( id, updatedValue) => {
+        if (!updatedValue.text || /^\s*$/.test(updatedValue.text)) {
+            return;
+        }
+
+        setTodos(prev => prev.map(item => (item.id === id ? updatedValue : item)));
 
     };
 
+  // function to check whether a todo item is completed or not
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if (todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    };
+
     return (
-        <div>
-            <h1> What you wanna do today?</h1>
-            <TodoForm onSubmit={addTodo}/>
-   
-        </div>
+        <>
+            <h1>What's the Plan for Today?</h1>
+            <TodoForm onSubmit={addTodo} />
+            <Todo
+                todos={todos}
+                completeTodo={completeTodo}
+                removeTodo={removeTodo}
+                updateTodo={updateTodo}
+            />
+        </>
     );
 }
 
